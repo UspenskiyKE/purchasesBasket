51c73193959a44a1a107c269d5c1b37c22eb6846
Кирилл <uspenskiyke@mail.ru>
2022-08-21 19:46:26 +0400

    Домашняя работа.

--- src/main/java/com/flightofdream/purchasesBasket/BasketServiceImpl.java
+++ src/main/java/com/flightofdream/purchasesBasket/BasketServiceImpl.java
@@ -1,4 +1,20 @@
 package com.flightofdream.purchasesBasket;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.IntStream;
 
-public class BasketServiceImpl {
+@Service
+@Scope(scopeName = "prototype")
+public class BasketServiceImpl implements BasketService{
+  ArrayList<Integer> purchases=new ArrayList<>();
+  private final   Basket basket=new Basket(purchases);
+    public void addPurchase(int... purchaseNumbers) {
+        Integer [] integerArray = IntStream.of(purchaseNumbers).boxed().toArray(Integer []::new);
+        Collections.addAll(purchases, integerArray);
+    }
+    public ArrayList getPurchases() {
+        return basket.getPurchases();
+    }
 }
